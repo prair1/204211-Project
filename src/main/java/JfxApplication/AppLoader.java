69a3285504fc0d6d6fa5af984aab3743112304d8
Meehoi <beam_baimon@hotmail.com>
2019-03-30 16:35:47 +0700

    Rename scene, Remake log
    - Rename scene to ready the multiple scene building
    - Remake log prevent 1 log file mess
    - also, gradle now build in utf-8 (bc Thai text not show properly)

--- src/main/java/JfxApplication/AppLoader.java
+++ src/main/java/JfxApplication/AppLoader.java
@@ -11,7 +11,7 @@ public class AppLoader extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("setupScene.fxml"));
 
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
